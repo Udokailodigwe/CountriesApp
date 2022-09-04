f889765c5c469e43f95b4ca922cb4f4646e5f2b0
Udokailodigwe <ilodigweudoka@gmail.com>
2022-09-04 04:37:44 +0300

    Fixed Switch based on specific routes

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import { Switch, Route } from 'react-router-dom'
 
+import Country from './components/Country'
+import Favorite from './components/FavoriteList/Favorite'
 import Home from './components/Home'
 
 const Routes = () => {
   return (
     <div>
       <Switch>
-        <Route exact path="/" component={Home} />
+        <Route path="/country/:id" component={Country} />
+        <Route path="/favorite" component={Favorite} />
+        <Route path="/" component={Home} />
       </Switch>
     </div>
   )
