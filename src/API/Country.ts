0d2efd6fdece155f49d1c24a86f20d941801a743
Udokailodigwe <ilodigweudoka@gmail.com>
2022-09-11 17:51:40 +0300

    Created API service file for fetching country data

--- src/API/Country.ts
+++ src/API/Country.ts
@@ -4,6 +4,7 @@ import { Country } from '../types'
 const fetchCountries = async () => {
   const URL =
     'https://restcountries.com/v3.1/all?fields=name,languages,currencies,population,flags,capital'
+
   const resData = await axios.get(URL)
   const cleanedData: Country[] = resData.data.map(
     (country: { name: { common: string } }) => ({
